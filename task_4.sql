86a1a339aa3e9244a382d5a7458544e5edbb5277
lovemuthoka <lovemuthoka3@gmail.com>
2024-07-27 20:34:23 +0300

    task4

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,10 @@
--- task_4.sql
+USE alx_book_store;
 
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'Books';
-
+#select all books in the database
+SELECT
+INFORMATION_SCHEMA.COLUMNS",
+ "COLUMN_NAME", 
+ "COLUMN_TYPE", 
+ "TABLE_SCHEMA = 'alx_book_store'",
+ "TABLE_NAME = 'Books'";
+  
